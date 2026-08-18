185eb60be719a11c28a163c7b3a9a5bfd4c84e98
Uros Zec <zecuros23@gmail.com>
2026-08-18 17:56:31 +0200

    Add Docker containerization and compose setup

--- services/MovieService/Program.cs
+++ services/MovieService/Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
